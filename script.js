dde895b6f295a52fbba8c9a4b9b68fc8c71e8e03
oliver.ht <oliver.hedlytreiberg@gmail.com>
2026-06-08 23:39:23 +0200

    mer små saker

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ function myfunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+function myfunction2() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
